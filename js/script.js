705e89e41075591a25c42cd4bb4d27058d54af8c
EliaPennoni <elia.pennoni1991@gmail.com>
2024-09-10 16:41:53 +0200

    esercizio terminato

--- js/script.js
+++ js/script.js
@@ -5,7 +5,17 @@ const { createApp } = Vue
 const app = createApp({
     data() {
         return {
-           
+           message: '',
+           emails:[]
+        };
+    },
+    created() {
+        for(let i =0; i<10; i++){
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((resp)=>{
+                this.emails.push(resp.data.response);
+            })
         }
     }
 }).mount('#app')
